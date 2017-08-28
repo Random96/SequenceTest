7eba4b5b3494ba6419417db0b6162250f4f1b794
Матвей Елин <m_elin@outlook.com>
2017-08-28 16:31:03 +0500

    fix ef threadsafe

--- EmlSoft.Sequence.UnitTestProject/SequenceTest.cs
+++ EmlSoft.Sequence.UnitTestProject/SequenceTest.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace EmlSoft.Sequence.UnitTestProject
 {
 	public class SequenceTest
 	{
-		[Fact]
-		public void SequenceTestMethd()
+		private void GetNum(List<string> List )
 		{
 			using (var context = new SequenceModel())
 			{
@@ -18,11 +19,36 @@ namespace EmlSoft.Sequence.UnitTestProject
 					if (sequence == null)
 						sequence = rep.CreateRepository(SequenceName, "ABC[ГОД][00000]-44", 1, 99999, 1, true);
 
-					var sec1 = sequence.CurrentValue();
-
-					var sec2 = sequence.NextValue();
+					for (int i = 0; i < 1000; ++i)
+						List.Add( sequence.NextValue() );
 				}
 			}
+		}
+
+		[Fact]
+		public void SequenceTestMethd()
+		{
+			List<string> [] res = new List<string>[10];
+
+			var thds = new Task[10];
+
+			for( int i=0; i<10; ++i )
+			{
+				var q = new List<string>();
+				res[i] = q;
+				thds[i] = Task.Factory.StartNew( ()=>GetNum(q) );
+			}
+
+			Task.WaitAll(thds);
+
+			var result = new HashSet<string>();
+
+			for ( int i = 0; i <10; ++i )
+			{
+				foreach (var s in res[i])
+					Assert.Equal<bool>(result.Add(s), true);
+			}
+
 		}
 
 		const string SequenceName = "Test";
